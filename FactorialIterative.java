9b7393955da8d4b9fd633dd6fcd535f860d20368
Saritha108 <168550587+Saritha108@users.noreply.github.com>
2024-05-05 17:21:57 +0530

    Add files via upload

---
+++ FactorialIterative.java
@@ -0,0 +1,24 @@
+package Prg;
+
+public class FactorialIterative {
+
+
+        // Method to find factorial of the given number
+        static int factorial(int n)
+        {
+            int res = 1, i;
+            for (i = 2; i <= n; i++)
+                res *= i;
+            return res;
+        }
+
+        // Driver method
+        public static void main(String[] args)
+        {
+            int num = 5;
+            System.out.println(
+                    "Factorial of " + num
+                            + " is " + factorial(5));
+        }
+
+}
---
+++ FactorialRecursive.java
@@ -0,0 +1,19 @@
+package Prg;
+
+public class FactorialRecursive {
+    static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    // Driver method
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num
+                + " is " + factorial(5));
+    }
+}
